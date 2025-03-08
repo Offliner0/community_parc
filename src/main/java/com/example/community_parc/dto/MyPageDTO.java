5ed2ba61961526f2130b9b3c38d438f556b99c71
leeingu <xltmsk@naver.com>
2025-03-08 15:54:48 +0900

    마이페이지 기능 추가

---
+++ src/main/java/com/example/community_parc/dto/MyPageDTO.java
@@ -0,0 +1,16 @@
+package com.example.community_parc.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class MyPageDTO {
+    String nickname;
+    LocalDateTime createdAt;
+    Page<PostResponseDTO> myPosts;
+    Page<CommentResponseDTO> myComments;
+}
